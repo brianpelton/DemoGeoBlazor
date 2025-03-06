bae4196deed4a11fa0891f734288a3e82a7758cf
Brian Pelton <brian.pelton@gmail.com>
2025-03-06 13:37:02 -0800

    Add api key from keyvault.

---
+++ MsBlazorAppTemplate/MsBlazorAppTemplate/ProgramExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
+
+namespace Gis.BlazorServerApp;
+
+public static class ProgramExtensions
+{
+    public static void AddSecretToConfiguration(this WebApplicationBuilder builder, string secretName, string? configurationName = null)
+    {
+        string keyVaultUrl = Environment.GetEnvironmentVariable("KEY_VAULT_URI");
+
+        // Authenticate using Managed Identity
+        var client = new SecretClient(new Uri(keyVaultUrl), new DefaultAzureCredential());
+
+        // Retrieve the secret
+        KeyVaultSecret secret = client.GetSecret(secretName);
+        string apiKey = secret.Value;
+        builder.Configuration[configurationName ?? secretName] = secret.Value;
+    }
+}
